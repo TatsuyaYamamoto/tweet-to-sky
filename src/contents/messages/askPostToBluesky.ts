e2894a1a13e493c84d15175d8d96e35db5cd0732
TatsuyaYamamoto <t28.is.tatsuya@gmail.com>
2024-02-15 11:00:30 +0900

    build: replace to zod

--- src/contents/messages/askPostToBluesky.ts
+++ src/contents/messages/askPostToBluesky.ts
@@ -1,4 +1,4 @@
-import { literal, object, safeParse, string, type Output } from "valibot";
+import { z } from "zod";
 
 // chrome の(型の)バグ？ (https://zwzw.hatenablog.com/entry/2019/12/04/200000)
 // Promise ではなく true を返さないと sendResponse の内容が background で受信できない (https://developer.mozilla.org/ja/docs/Mozilla/Add-ons/WebExtensions/API/runtime/onMessage)
@@ -8,11 +8,11 @@ type RuntimeMessageListener = (
   >
 ) => true | void;
 
-export const AskPostToBlueskyMessageSchema = object({
-  type: literal("askPostToBluesky"),
-  tweetId: string(),
+export const AskPostToBlueskyMessageSchema = z.object({
+  type: z.literal("askPostToBluesky"),
+  tweetId: z.string(),
 });
-export type AskPostToBlueskyMessage = Output<
+export type AskPostToBlueskyMessage = z.infer<
   typeof AskPostToBlueskyMessageSchema
 >;
 
@@ -20,13 +20,13 @@ export const onAskPostToBlueskyMessage = (
   callback: (message: AskPostToBlueskyMessage) => void,
 ) => {
   const lister: RuntimeMessageListener = (rawMessage): true | void => {
-    const parseResult = safeParse(AskPostToBlueskyMessageSchema, rawMessage);
+    const parseResult = AskPostToBlueskyMessageSchema.safeParse(rawMessage);
     if (!parseResult.success) {
       return;
     }
 
     console.log("[onMessage:background->tab(-)]", rawMessage);
-    callback(parseResult.output);
+    callback(parseResult.data);
   };
 
   chrome.runtime.onMessage.addListener(lister);
