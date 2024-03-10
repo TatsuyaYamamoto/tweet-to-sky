cdbd4a05e3f8b9173b25744a93d6938ac0736bfa
Tatsuya Yamamoto / T28_tatsuya <t28.is.tatsuya@gmail.com>
2024-03-10 22:38:32 +0900

    feat: compress images before posting to bluesky (#4)
    * chore: add deps
    
    * chore: add function

--- src/shared/helpers/utils.ts
+++ src/shared/helpers/utils.ts
@@ -1,3 +1,5 @@
+import imageCompression from "browser-image-compression";
+
 export const base64ToBinary = (base64: string) => {
   return Uint8Array.from(atob(base64), (c) => c.charCodeAt(0));
 };
@@ -10,4 +12,18 @@ export const arrayBufferToBase64 = (arrayBuffer: ArrayBuffer) => {
     binary += String.fromCharCode(byte);
   });
   return btoa(binary);
+};
+
+export const compressImage = async (
+  input: Uint8Array,
+  mediaType: string,
+  maxSizeMib: number, // https://github.com/Donaldcwl/browser-image-compression/blob/master/lib/image-compression.js#L51
+): Promise<Uint8Array> => {
+  const rawFile = new File([input.buffer], "image", {
+    type: mediaType,
+  });
+  const compressedFile = await imageCompression(rawFile, {
+    maxSizeMB: maxSizeMib,
+  });
+  return new Uint8Array(await compressedFile.arrayBuffer());
 };
